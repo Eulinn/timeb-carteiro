2a87cfe9b750b63440968255a8a3b29490c26f17
isadoraAlexandre <isadoraalexandre489@gmail.com>
2024-04-30 21:47:33 -0300

    Cria estrutura da caixa

--- src/components/caixa.rs
+++ src/components/caixa.rs
@@ -1,14 +1,23 @@
-pub struct Caixa{
-
-    
+enum Status{
+    Menu,
+    ComCarteiro,
+    SemCarteiro
 }
 
+pub struct Caixa{
+    pos_x: i32,
+    pos_y: i32,
+    status: Status
+}
 
 impl Caixa {
     pub fn new() -> Self{
         //aqui tem que retornar o objeto da Caixa
 
-        Caixa {}
+        Caixa {
+            pos_x: 0,
+            pos_y: 0,
+            status: Status::Menu
+        }
     }
-    
 }
